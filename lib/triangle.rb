ed1aa718f8decd70f1306ce1df5a9dabba863098
Kevin Downs <kevin.downs04@gmail.com>
2019-11-22 06:22:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,64 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3, :type
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind 
+    case
+      when self.equilateral? && self.valid?
+        set_type(:equilateral) 
+      when self.isosceles? && self.valid?
+        set_type(:isosceles)
+      when self.scalene? && self.valid?
+        set_type(:scalene)
+      else
+        raise TriangleError
+      end
+    self.type
+  end
+  
+  def equilateral?
+    #equilateral triangles have equal sides
+    self.side1 == self.side2 && self.side2 == self.side3
+  end
+  
+  def isosceles?
+    #isosceles triangles have two sides equal
+    ((self.side1 == self.side2) && (self.side2 != self.side3)) ||
+    ((self.side2 == self.side3) && (self.side3 != self.side1)) ||
+    ((self.side3 == self.side1) && (self.side1 != self.side2))
+  end
+  
+  def scalene?
+    #scalene triangles have no equal sides
+    (self.side1 != self.side2) && (self.side2 != self.side3) && (self.side3 != self.side1) 
+  end
+  
+  def valid?
+    if (self.side1 + self.side2 + self.side3) == 0
+      return false
+    end
+    ((self.side1 + self.side2) > self.side3) &&
+    ((self.side2 + self.side3) > self.side1) &&
+    ((self.side3 + self.side1) > self.side2)
+  end
+  
+  def set_type(type)
+    @type = type
+  end
+
+  class TriangleError < StandardError
+    
+    def message
+      "Invalid parameters!! Lengths violate triangle inequality"
+    end
+    
+  end
+  
 end
